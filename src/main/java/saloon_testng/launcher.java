9a3c09b8a0ff6fd695a437ab6603dce8d5bb895d
sumitavala <sumitavala.online@gmail.com>
2022-02-12 17:46:26 +0530

    first commit

---
+++ src/main/java/saloon_testng/launcher.java
@@ -0,0 +1,10 @@
+package saloon_testng;
+
+public class launcher {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("My first test ng project");
+	}
+
+}
